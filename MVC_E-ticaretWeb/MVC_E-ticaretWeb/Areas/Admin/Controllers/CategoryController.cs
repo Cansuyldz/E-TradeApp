643c4574e787ce09b432843dd230483b51020e8a
Cansu Aydemir <yldz.cansu16@gmail.com>
2025-03-19 22:16:50 +0300

    .

---
+++ MVC_E-ticaretWeb/MVC_E-ticaretWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_E_ticaretWeb.Models;
+
+namespace MVC_E_ticaretWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public DataBaseContext _context = new();
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+           
+                if (ModelState.IsValid)
+                {
+                    _context.Categories.Add(category); 
+                    _context.SaveChanges(); 
+                    return RedirectToAction("Index");
+                }
+                return View(category);
+            
+
+        }
+
+    }
+}
